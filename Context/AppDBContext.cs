8e0fa0d665718506ca6120e6fb2f920e69b9d27b
Eosoft89 <ericrojas.ing@gmail.com>
2024-09-20 15:42:48 -0300

    Agregar archivos de proyecto.

---
+++ Context/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonAPIASP.Models;
+
+namespace PersonAPIASP.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        
+
+    }
+}
